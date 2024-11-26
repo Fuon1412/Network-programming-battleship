0f24b33818e8668b31ca058c4303713966ce1800
Phuong Do Manh <phuong.dm225660@sis.hust.edu.vn>
2024-11-26 15:55:38 +0700

    add room class for room service

---
+++ battleship/src/main/java/src/server/models/Room.java
@@ -0,0 +1,13 @@
+package src.server.models;
+
+import lombok.*;
+import java.util.List;
+
+@Getter
+@Setter
+public class Room {
+    private int roomId;
+    private String roomName;
+    private boolean isPlaying;
+    private List<Player> players;
+}
